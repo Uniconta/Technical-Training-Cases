fca071d723080976cbac512f1605094c2ab97e1a
Rasmus Aaberg Hansen <rh@zendo.dk>
2018-01-14 15:44:39 +0100

    Refactoring to Rev2

---
+++ Case3/UnicontaWrapper/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using TableCreator.Core.Managers;
+using Uniconta.Common;
+
+namespace UnicontaWrapper
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            UnicontaAPIManager.Initialize();
+
+            // TODO: Enter username and password
+            var errorCode = UnicontaAPIManager.Login("USERNAME", "PASSWORD").Result;
+            if (errorCode != ErrorCodes.Succes)
+            {
+                MessageBox.Show("ERROR: Login Failed" + errorCode.ToString());
+                System.Windows.Application.Current.Shutdown();
+                return;
+            }
+
+            UnicontaAPIManager.InitializeCompanies().Wait();
+        }
+    }
+}

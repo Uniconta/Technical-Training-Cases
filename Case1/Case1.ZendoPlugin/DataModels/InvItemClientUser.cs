79ab8a9c664965579dc9e098eb5ad076454e872a
Rasmus Aaberg Hansen <rh@sit.dk>
2018-03-13 13:48:30 +0100

    Started work on rev4

---
+++ Case1/Case1.ZendoPlugin/DataModels/InvItemClientUser.cs
@@ -0,0 +1,19 @@
+﻿using Uniconta.ClientTools.DataModel;
+
+namespace Case1.ZendoPlugins.DataModels
+{
+    public class InvItemClientUser : InvItemClient
+    {
+        public string BarcodeNumber
+        {
+            get { return this.GetUserFieldString(0); }
+            set { this.SetUserFieldString(0, value); }
+        }
+
+        public long SpeakerScore
+        {
+            get { return this.GetUserFieldInt64(1); }
+            set { this.SetUserFieldInt64(1, value); }
+        }
+    }
+}
---
+++ Case2/Case2.ZendoPlugin/Models/LastFMGetInfoResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Case2.ZendoPlugins.Models
+{
+    public class LastFMGetInfoResponse
+    {
+        public LastFMTrack Track { get; set; }
+    }
+}

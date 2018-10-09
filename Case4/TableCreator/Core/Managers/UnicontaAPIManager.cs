b1b8bb8686d6ac02fb98445178c72108962e44bf
AlexBanks97 <AlexBanks97@users.noreply.github.com>
2018-10-09 10:48:32 +0200

    Update UnicontaAPIManager.cs

--- Case4/TableCreator/Core/Managers/UnicontaAPIManager.cs
+++ Case4/TableCreator/Core/Managers/UnicontaAPIManager.cs
@@ -27,7 +27,7 @@ namespace TableCreator.Core.Managers
                 throw new InvalidOperationException();
 
             // TODO: Change API Key
-            accessGuid = new Guid("e7cb2d3d-3045-4102-ab26-6483e3b2be1e");
+            accessGuid = new Guid(00000000-0000-0000-0000-000000000000");
             unicontaConnection = new UnicontaConnection(APITarget.Live);
             unicontaSession = new Session(unicontaConnection);
         }
